5c1a91a66acdd34c80c3793827e1e602153c500d
Atanas Janeshliev <atanas.janeshliev@proton.ch>
2025-09-11 18:05:46 +0200

    chore: remove hardcoded header paths

--- fido2_static_amd64.go
+++ fido2_static_amd64.go
@@ -1,7 +1,9 @@
 package libfido2
 
 /*
-#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/lib/libfido2.a /usr/local/opt/openssl@3/lib/libcrypto.a ${SRCDIR}/darwin/amd64/lib/libcbor.a
-#cgo darwin CFLAGS: -I/usr/local/opt/libfido2/include -I/usr/local/opt/openssl@3/include
+#cgo darwin,!libfido2_system LDFLAGS: -framework CoreFoundation -framework IOKit
+#cgo darwin,!libfido2_system CFLAGS:
+#cgo darwin,libfido2_system LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/lib/libfido2.a /usr/local/opt/openssl@3/lib/libcrypto.a ${SRCDIR}/darwin/amd64/lib/libcbor.a
+#cgo darwin,libfido2_system CFLAGS: -I/usr/local/opt/libfido2/include -I/usr/local/opt/openssl@3/include
 */
 import "C"
